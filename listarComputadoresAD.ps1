efdec0c2575ecf4d2ca20fde1b3b3a5edcfeb68c
Jouderian Nobre <jouderian@gmail.com>
2025-03-20 16:33:58 -0300

    Incluindo a ultima alteração da maquina no AD

--- listarComputadoresAD.ps1
+++ listarComputadoresAD.ps1
@@ -3,6 +3,7 @@
 # Versao 1 (06/02/23) Jouderian Nobre
 # Versao 2 (18/03/24) Jouderian Nobre: Melhoria para extracao dos dados
 # Versao 3 (29/12/24) Jouderian Nobre: Passa a ler a variavel do Windows para local do arquivo
+# Versao 4 (20/03/25) Jouderian Nobre: Incluindo a ultima alteração da maquina no AD
 #--------------------------------------------------------------------------------------------------------
 
 Clear-Host
@@ -11,15 +12,16 @@ Import-Module ActiveDirectory
 $indice = 0
 $inicio = Get-Date
 $periodo = $inicio.AddDays(-90)
+$situacao = ""
 $arquivo = "$($env:ONEDRIVE)\Documentos\WindowsPowerShell\computadoresAD.csv"
 
 Write-Host `n`n`n`n`n
 Write-Host Inicio: $inicio
 Write-Host Consultado computadores no AD...
-$computadoresAD = Get-ADComputer -Filter * -Properties Name,LastLogonDate,OperatingSystem,OperatingSystemVersion,IPv4Address,IPv6Address,DNSHostName,DistinguishedName,LastUser
+$computadoresAD = Get-ADComputer -Filter * -Properties Name,LastLogonDate,Modified,OperatingSystem,OperatingSystemVersion,IPv4Address,IPv6Address,DNSHostName,DistinguishedName
 $total = $computadoresAD.Count
 
-Write-Output "Nome,ultimoAcesso,sistemaOperacional,versaoSO,IPv4,IPv6,DNSHostName,nomeDistinto" > $arquivo
+Write-Output "Nome,ultimoAcesso,ultimaModificacao,sistemaOperacional,versaoSO,IPv4,IPv6,DNSHostName,nomeDistinto,situacao" > $arquivo
 
 foreach ($computador in $computadoresAD){
   $indice++
@@ -27,10 +29,15 @@ foreach ($computador in $computadoresAD){
 
 #  $ultimoUsuario = Get-WinEvent -ComputerName $computador.Name -LogName Security -FilterXPath "*[System[EventID=4624]]" | Select-Object -First 1 -Property @{Name="User";Expression={$_.Properties[5].Value}}
 
-  $nomeDistinto = [System.String]::Concat("""","$($computador.DistinguishedName)",""",")
+if($computador.Modified -cgt $periodo){
+  $situacao = "Normal"
+} else {
+  $situacao = "Expirado"
+}
+$nomeDistinto = [System.String]::Concat("""","$($computador.DistinguishedName)",""",",$situacao)
 
 #  Echo "$($computador.Name),$($computador.LastLogonDate.ToString('dd/MM/yy HH:mm')),$($computador.OperatingSystem),$($computador.OperatingSystemVersion),$($computador.IPv4Address),$($computador.IPv6Address),$($computador.DNSHostName),$($nomeDistinto),$($ultimoUsuario.User)" >> $arquivo
-  Write-Output "$($computador.Name),$($computador.LastLogonDate.ToString('dd/MM/yy HH:mm')),$($computador.OperatingSystem),$($computador.OperatingSystemVersion),$($computador.IPv4Address),$($computador.IPv6Address),$($computador.DNSHostName),$($nomeDistinto)" >> $arquivo
+  Write-Output "$($computador.Name),$($computador.LastLogonDate.ToString('dd/MM/yy HH:mm')),$($computador.Modified.ToString('dd/MM/yy HH:mm')),$($computador.OperatingSystem),$($computador.OperatingSystemVersion),$($computador.IPv4Address),$($computador.IPv6Address),$($computador.DNSHostName),$($nomeDistinto)" >> $arquivo
 }
 
 Write-Progress -Activity "Listando computadores" -PercentComplete 100
