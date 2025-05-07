aa724ae2b4dad47fab485b9d76101a44ce01b01d
Jouderian Nobre <jouderian@gmail.com>
2025-05-07 13:05:42 -0300

    Melhoria na exibicao dos dados

--- listarMembrosGrupos_EntraID.ps1
+++ listarMembrosGrupos_EntraID.ps1
@@ -1,10 +1,12 @@
 ﻿#--------------------------------------------------------------------------------------------------------
 # Descricao: Mostra os membros de um grupo do EntraID
-# Versao 1 (06/03/25) Jouderian
+# Versao 1 (06/03/25) Jouderian Nobre
+# Versao 2 (07/05/25) Jouderian Nobre: Melhoria na exibicao dos dados
 #--------------------------------------------------------------------------------------------------------
 
 Clear-Host
 
+#--------------------------------------------------------------------- VARIAVEIS
 $inicio = Get-Date
 $groupName = "semMFA.M365"
 
@@ -18,8 +20,12 @@ if ($group){
     # Obtenha os membros do grupo
     $members = Get-AzureADGroupMember -ObjectId $group.ObjectId
 
-    # Exiba os membros
-    $members | Select-Object DisplayName, UserPrincipalName
+    foreach ($member in $members){
+        $user = Get-AzureADUser -ObjectId $member.ObjectId
+        $situacao = if ($user.AccountEnabled) { "Ativa" } else { "Bloqueada" }
+        Write-Output "$($user.DisplayName),$($user.UserPrincipalName),$($user.UserType),$($situacao)"
+    }
+
     Write-Output "`n`nTotal de membros: $($members.Count)"
 } else {
     Write-Output "Grupo não encontrado: $groupName"
