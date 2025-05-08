abf19c71725ade57929232773d98f0358cce0be5
Jouderian Nobre <jouderian@gmail.com>
2025-05-08 01:08:48 -0300

    Ajuste para melhorar o tratamento de erros e rodar so servidor

--- activeDirectory/listarCredenciaisAD.ps1
+++ activeDirectory/listarCredenciaisAD.ps1
@@ -1,109 +1,94 @@
-﻿#--------------------------------------------------------------------------------------------------------
-# Descricao: Script para gerar um arquivo .csv com as principais informcoes dos usuarios do AD.
-# Versao 1 (06/12/22) Savio
-# Versao 2 (04/05/23) Jouderian Nobre: Melhoria no tratamento do ultimo acesso
-# Versao 3 (08/05/23) Jouderian Nobre: Inclusão de mais informações da credencial
-# Versao 4 (07/11/24) Jouderian Nobre: Inclusáo da biblioteca, tratamento dos campos e melhoria no acompanhamento da progressao da extracao
-# Versao 5 (29/12/24) Jouderian Nobre: Passa a ler a variavel do Windows para local do arquivo
-# Versao 6 (17/03/25) Jouderian Nobre: Incluir o campo POBox para identificar sincronismo com o M365
+﻿#-------------------------------------------------------------------------------
+# Descricao: Script para atualizar os dados das credenciais com base na QLP do RH
+# Versao 1 (25/07/23) Jouderian Nobre
+# :::
+# Versao 4 (16/03/24) Jouderian Nobre: Melhoria no ajuste de mudanca de mes e inclusao de barra de progresso
+# Versao 5 (20/08/24) Jouderian Nobre: Melhoria no tratamento dos caracteres dos campos de texto
+# Versao 6 (05/10/24) Jouderian Nobre: Ajuste para tratar gestores com mais de 20 caracteres
+# Versao 7 (29/12/24) Jouderian Nobre: Passa a ler a variavel do Windows para local do arquivo
+# Versao 8 (07/05/25) Jouderian Nobre: Ajuste para melhorar o tratamento de erros e rodar so servidor
 #--------------------------------------------------------------------------------------------------------
 
-. "$($env:ONEDRIVE)\Documentos\WindowsPowerShell\Scripts\Publico\Microsoft-365\bibliotecaDeFuncoes.ps1"
+. "C:\ScriptsRotinas\bibliotecas\bibliotecaDeFuncoes.ps1"
 
 Clear-Host
 
 $indice = 0
 $inicio = Get-Date
-$arquivo = "$($env:ONEDRIVE)\Documentos\WindowsPowerShell\listaUsuariosAD.csv"
+$versao = "2025.04"
+$arquivoUsuarios = "C:\ScriptsRotinas\atualizaCredenciaisAD\QLP_$($versao).xlsx"
+$arquivoLog = "C:\ScriptsRotinas\atualizaCredenciaisAD\Logs\syncQLP_$($inicio.ToString('yyMMdd_HHmmss')).csv"
+$mensagem = "Atualizado pela QLP $($versao) em $($inicio.ToString('dd/MM/yy HH:mm'))"
+
+$Modules = Get-Module -Name ImportExcel -ListAvailable
+if($Modules.count -eq 0){
+  Write-Host Instale o modulo do ImportExcel usando o comando abaixo:`n  Install-Module ImportExcel -ForegroundColor yellow
+  Exit
+}
+Import-Module ImportExcel
 
 Write-Host "`n`n`n`n`n`n`nInicio:" $inicio
-Write-Host Pesquisando relacao de credenciais no AD...
-$credenciais = Get-ADUser -Filter * `
-  -Properties `
-    SamAccountName, `
-    DisplayName, `
-    EmailAddress, `
-    Enabled, `
-    POBox, `
-    Company, `
-    Office, `
-    Department, `
-    Title, `
-    Manager, `
-    Created, `
-    LastLogonDate, `
-    LastLogonTimestamp, `
-    AccountExpirationDate, `
-    PasswordLastSet, `
-    CanonicalName, `
-    ObjectGUID
+$usuarios = Import-Excel -Path $arquivoUsuarios -WorksheetName "Relacao"
 
-Out-File -FilePath $arquivo -InputObject "Credencial;Nome;eMail;Ativa;M365;Empresa;Escritorio;Departamento;Cargo;Gerente;Criacao;ultimoAcesso;Expiracao;mudancaSenha;CanonicalName;ObjectGUID;Grupos" -Encoding UTF8
-$totalCredenciais = $credenciais.Count
+Out-File -FilePath $arquivoLog -InputObject "contaAD,nome,cidade,empresa,filial,departamento,cargo,gestor,codCC,nomeCC,situacao" -Encoding UTF8
+$totalUsuarios = $usuarios.Count
 
-Foreach ($credencial in $credenciais){
+Foreach ($usuario in $usuarios){
 
   $indice++
+  $situacao = ""
 
-  if ($indice % 10 -eq 0){ # Atualiza o progresso a cada 10 caixas processadas
-    Write-Progress -Activity "Coletando dados das credenciais" -Status "Progresso: $indice de $totalCredenciais coletadas" -PercentComplete (($indice / $totalCredenciais) * 100)
+  Write-Progress -Activity "Atualizando credenciais" -Status "Progresso: $indice de $totalUsuarios atualizados" -PercentComplete (($indice / $totalUsuarios) * 100)
+
+#--------------------------------------------------------------- Tratando campos
+  $nome = trataTexto -texto $usuario.nomeFuncionario -notacao "C"
+  $empresa = trataTexto -texto $usuario.Empresa -notacao "C"
+  $escritorio = trataTexto -texto $usuario.Escritorio -notacao "C"
+  $departamento = trataTexto -texto $usuario.Departamento -notacao "C"
+  $departamento = $departamento.replace('Sac', 'SAC')
+  $cargo = trataTexto -texto $usuario.Cargo -notacao "C"
+  $cargo = $cargo.replace(' Vi', ' VI')
+  $cidade = trataTexto -texto $usuario.cidade -notacao "C"
+  $centroDeCusto = trataTexto -texto $usuario.centroDeCusto -notacao "C"
+  $gestor = $usuario.contaADgestor
+  $gestor = $gestor.Substring(0, [Math]::Min(20, $gestor.Length))
+
+#-------------------------------------------------------- Atualizando credencial
+  try {
+    $usuarioAD = Get-ADUser -Identity $Usuario.contaAD -Property enabled,description
+  } catch {
+    $situacao = $_.Exception.Message
+    Out-File -FilePath $arquivoLog -InputObject "$($usuario.contaAD),$($nome),$($cidade),$($empresa),$($escritorio),$($Departamento),$($Cargo),$($gestor),$($usuario.codCC),$($centroDeCusto),$($situacao)" -Encoding UTF8 -append
+    Continue
   }
 
-#  $grupos = Get-ADPrincipalGroupMembership -Identity $credencial.SamAccountName
-
-  $departamento = if($null -ne $credencial.Department){ removeQuebraDeLinha -texto $credencial.Department }
-  $cargo = if($null -ne $credencial.Title){ removeQuebraDeLinha -texto $credencial.Title }
-
-  $infoCredencial = "$($credencial.SamAccountName);" # Credencial
-  $infoCredencial += "$($credencial.DisplayName);" # Nome
-  $infoCredencial += "$($credencial.EmailAddress);" # eMail
-  $infoCredencial += "$($credencial.Enabled);" # Ativa
-  $infoCredencial += "$($credencial.POBox);" # M365
-  $infoCredencial += "$($credencial.Company);" # Empresa
-  $infoCredencial += "$($credencial.Office);" # Escritorio
-  $infoCredencial += "$($departamento);" # Departamento
-  $infoCredencial += "$($cargo);" # Cargo
-  $infoCredencial += [System.String]::Concat("""","$($credencial.Manager)",""";") # Gerente
-
-  $momento = $credencial.Created
-  $infoCredencial += "$($momento.ToString('dd/MM/yy HH:mm'));" # Criacao
-
-  $momento = $credencial.LastLogonDate
-  $momento2 = [DateTime]::FromFileTimeUtc($credencial.LastLogonTimestamp)
-  If($momento -gt $momento2){ # ultimoAcesso
-    $infoCredencial += "$($momento.ToString('dd/MM/yy HH:mm'));"
+  If ($usuarioAD.Enabled){
+    try {
+      Set-ADUser -Identity $usuario.contaAD `
+      -DisplayName $nome `
+      -City $cidade `
+      -Company $empresa `
+      -Office $escritorio `
+      -Department $departamento `
+      -Title $cargo `
+      -Manager $gestor `
+      -streetAddress $centroDeCusto `
+      -postalCode $usuario.codCC `
+      -Replace @{info=$mensagem} `
+      -Country "BR"
+      $situacao = "Atualizado"
+    } catch {
+      $situacao = $_.Exception.Message
+      Out-File -FilePath $arquivoLog -InputObject "$($usuario.contaAD),$($nome),$($cidade),$($empresa),$($escritorio),$($Departamento),$($Cargo),$($gestor),$($usuario.codCC),$($centroDeCusto),$($situacao)" -Encoding UTF8 -append
+      Continue
+    }
   } else {
-    $infoCredencial += "$($momento2.ToString('dd/MM/yy HH:mm'));"
+    $situacao = $usuarioAD.description
   }
-
-  $momento = $credencial.AccountExpirationDate
-  if($null -eq $momento){ # Expiracao
-    $infoCredencial += ";"
-  } Else {
-    $infoCredencial += "$($momento.ToString('dd/MM/yy HH:mm'));"
-  }
-
-  $momento = $credencial.PasswordLastSet
-  if($null -eq $momento){ # mudancaSenha
-    $infoCredencial += ";"
-  } Else {
-    $infoCredencial += "$($momento.ToString('dd/MM/yy HH:mm'));"
-  }
-
-  $infoCredencial += "$($credencial.CanonicalName);" # CanonicalName
-  $infoCredencial += "$($credencial.ObjectGUID);" # ObjectGUID
-
-#  $texto = '"'
-#  Foreach ($grupo in $grupos){
-#    $texto += $grupo.name + "+"
-#  }
-#  $texto += '"'
-#  $infoCredencial += "$($texto)"
-
-  Out-File -FilePath $arquivo -InputObject $infoCredencial -Encoding UTF8 -append
+  Out-File -FilePath $arquivoLog -InputObject "$($usuario.contaAD),$($nome),$($cidade),$($empresa),$($escritorio),$($Departamento),$($Cargo),$($gestor),$($usuario.codCC),$($centroDeCusto),$($situacao)" -Encoding UTF8 -append
 }
 
-Write-Progress -Activity "Coletando dados das credenciais" -PercentComplete 100
+Write-Progress -Activity "Atualizando credenciais" -PercentComplete 100
 
 $final = Get-Date
 Write-Host `nInicio: $inicio
